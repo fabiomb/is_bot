2768a0dfe048032b71e63b0bfccf57756f55931d
Fabio <fabiomb@gmail.com>
2018-07-03 18:05:57 -0300

    más bots agregados

--- is_bot.php
+++ is_bot.php
@@ -3,51 +3,51 @@
 function is_bot($sistema){
  // bots de buscadores
     $bots = array(
-        'Googlebot',
-        'Baiduspider',
-        'ia_archiver',
-        'R6_FeedFetcher',
-        'NetcraftSurveyAgent',
-        'Sogou web spider',
-        'bingbot',
-        'Yahoo! Slurp',
-        'facebookexternalhit',
-        'PrintfulBot',
-        'msnbot',
-        'Twitterbot',
-        'UnwindFetchor',
-        'urlresolver',
-        'Butterfly',
-        'TweetmemeBot',
-        'PaperLiBot',
-        'MJ12bot',
-        'AhrefsBot',
-        'Exabot',
-        'Ezooms',
-        'YandexBot',
-        'SearchmetricsBot',
-        'picsearch',
-        'TweetedTimes Bot',
-        'QuerySeekerSpider',
-        'ShowyouBot',
-        'woriobot',
-        'merlinkbot',
-        'BazQuxBot',
-        'Kraken',
-        'SISTRIX Crawler',
-        'R6_CommentReader',
-        'magpie-crawler',
-        'GrapeshotCrawler',
-        'PercolateCrawler',
-        'MaxPointCrawler',
-        'R6_FeedFetcher',
-        'NetSeer crawler',
-        'grokkit-crawler',
-        'SMXCrawler',
-        'PulseCrawler',
-        'Y!J-BRW',
-        '80legs.com/webcrawler',
-        'Mediapartners-Google'
+        , 'Googlebot'
+        , 'Baiduspider'
+        , 'ia_archiver'
+        , 'R6_FeedFetcher'
+        , 'NetcraftSurveyAgent'
+        , 'Sogou web spider'
+        , 'bingbot'
+        , 'Yahoo! Slurp'
+        , 'facebookexternalhit'
+        , 'PrintfulBot'
+        , 'msnbot'
+        , 'Twitterbot'
+        , 'UnwindFetchor'
+        , 'urlresolver'
+        , 'Butterfly'
+        , 'TweetmemeBot'
+        , 'PaperLiBot'
+        , 'MJ12bot'
+        , 'AhrefsBot'
+        , 'Exabot'
+        , 'Ezooms'
+        , 'YandexBot'
+        , 'SearchmetricsBot'
+        , 'picsearch'
+        , 'TweetedTimes Bot'
+        , 'QuerySeekerSpider'
+        , 'ShowyouBot'
+        , 'woriobot'
+        , 'merlinkbot'
+        , 'BazQuxBot'
+        , 'Kraken'
+        , 'SISTRIX Crawler'
+        , 'R6_CommentReader'
+        , 'magpie-crawler'
+        , 'GrapeshotCrawler'
+        , 'PercolateCrawler'
+        , 'MaxPointCrawler'
+        , 'R6_FeedFetcher'
+        , 'NetSeer crawler'
+        , 'grokkit-crawler'
+        , 'SMXCrawler'
+        , 'PulseCrawler'
+        , 'Y!J-BRW'
+        , '80legs.com/webcrawler'
+        , 'Mediapartners-Google'
         , 'Spinn3r'
         , 'InAGist'
         , 'Python-urllib'
